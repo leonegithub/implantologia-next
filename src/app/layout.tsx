632096e15d6f9f9a909d53bf6348a70193f6f250
leonegithub <webmaster@leone.it>
2025-10-02 09:09:48 +0200

    Pixel Meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,28 @@ export default function RootLayout({
           referrerPolicy="no-referrer"
         />
         <link rel="stylesheet" href="https://use.typekit.net/qxk4lzc.css" />
+
+
+
+       
+        {/* Meta Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '788727910402752');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+     
+
+
       </head>
 
       <body>
